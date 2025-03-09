a0c71218fc85c680c2b9bc02af8ded8e7dd00fab
AMansour5 <ahmedmonsour5@icloud.com>
2025-03-10 00:24:06 +0200

    finaaaallly I/O handling done

--- Lab2/matrixMultiplication.c
+++ Lab2/matrixMultiplication.c
@@ -4,15 +4,26 @@
 int main(int argc, char *argv[]){
     struct timeval start, stop;
     gettimeofday(&start, NULL);
-    char *output_matrix_name = (argc > 3) ? strdup(argv[3]) : "c";
-    char *file2 = (argc > 2) ? strdup(argv[2]) : "b";
-    char *file1 = (argc > 1) ? strdup(argv[1]) : "a";
-    if (argc != 4 && argc != 1) {
-        fprintf(stderr, "Wrong input");
+    char *output_matrix_name;
+    char *file2_name;
+    char *file1_name;
+
+    if (argc == 4){
+        output_matrix_name = strdup(argv[3]);
+        file2_name = strdup(argv[2]);
+        file1_name = strdup(argv[1]);
+    }
+    else if (argc == 1) {
+        output_matrix_name = strdup("c");
+        file2_name = strdup("b");
+        file1_name = strdup("a");
+    }
+    else {
+        fprintf(stderr, "Wrong input\n");
         return EXIT_FAILURE;
     }
-    Matrix matrix_2 = readMatrix(file2);
-    Matrix matrix_1 = readMatrix(file1);
+    Matrix matrix_2 = readMatrix(file2_name);
+    Matrix matrix_1 = readMatrix(file1_name);
     writeMatrix(&matrix_1, output_matrix_name);
     gettimeofday(&stop, NULL);
     printf("Seconds taken %lu\n", stop.tv_sec - start.tv_sec);
